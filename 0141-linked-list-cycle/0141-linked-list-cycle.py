1ccebef38725aec9e5ca38149593b42a3e06f49e
Sruthi G <sruthiganesh05@gmail.com>
2025-06-02 20:41:01 +0530

    Time: 41 ms (33.91%), Space: 19.6 MB (60.67%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next        
+            fast = fast.next.next   
+            
+            if slow == fast:
+                return True  
+        
+        return False  
