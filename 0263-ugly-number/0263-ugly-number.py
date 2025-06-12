6c13a85ea9e53ad039c20dd377fd69ee0d147fbf
Sruthi G <sruthiganesh05@gmail.com>
2025-06-12 22:05:49 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.27%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False 
+
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n //= p
+
+        return n == 1
+        
