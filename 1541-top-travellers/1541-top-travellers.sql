78d2ce75b9bfc9d34d37d2469e1c21c5760e1841
Sruthi G <sruthiganesh05@gmail.com>
2025-06-17 21:45:02 +0530

    Time: 1013 ms (15.1%), Space: 0B (100%) - LeetHub

---
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select u.name, coalesce(sum(r.distance), 0) as travelled_distance from users u left join rides r on u.id = r.user_id group by u.id order by travelled_distance desc, u.name;
