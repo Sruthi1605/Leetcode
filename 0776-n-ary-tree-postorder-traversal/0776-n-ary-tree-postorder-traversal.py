bb6ae7b251c1e6a18d127f96a79ecc38177db463
Sruthi G <sruthiganesh05@gmail.com>
2025-07-20 21:23:28 +0530

    Time: 39 ms (34.6%), Space: 15.6 MB (52.06%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node(object):
+	def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            for child in node.children:
+                dfs(child)
+            res.append(node.val)
+
+        dfs(root)
+        return res
