fed952f6f29e274b693e450bbc781870c762773c
Sruthi G <sruthiganesh05@gmail.com>
2025-07-29 21:27:26 +0530

    Time: 77 ms (30.9%), Space: 20.5 MB (45.15%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        increasing = decreasing = True
+        
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                decreasing = False
+            if nums[i] < nums[i - 1]:
+                increasing = False
+        
+        return increasing or decreasing
